87c14818676dbeabb08724db3af9234ef7113a3f
sagopun <santiagozapatas@gmail.com>
2022-05-25 14:51:00 -0300

    version final

--- formulario.js
+++ formulario.js
@@ -1,42 +1,18 @@
 const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
+var nombre = document.getElementsByName('nombre');
+var error = document.getElementsByName ('error');
 
-const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	peso: /^\d{1,2}$/ // 7 a 14 numeros.
+function enviarFormulario()
+{
 
+    var mensajesError= [];
+
+    if(nombre.value === null || nombre.value === '')
+    {
+        mensajesError.push('ingresa un nombre valido');
+    }   
+
+    error.innerHTML = mensajesError.join ('. ');
+
+    return false;
 }
-
-const campos = {
-	nombre: false,
-	correo: false,
-	peso: false
-}
-
-
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
-
-
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
-
-	const terminos = document.getElementById('terminos');
-	if(campos.nombre && campos.correo && campos.peso && terminos.checked ){
-		formulario.reset();
-
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
-		}, 5000);
-
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
-		});
-	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
-	}
-});
